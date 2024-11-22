ee9e935e2c4cdfce25d451751069ab727135c5a9
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-11-22 12:57:22 +0200

    finished how to and footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import { Check, Star } from "lucide-react";
-import MaxWidthWrapper from "./components/MaxWidthWrapper";
-import TShirt from "./components/TShirt";
+import { Check, Star, ArrowRight } from "lucide-react";
 import { Icons } from "./components/Icons";
 import { Reviews } from "./components/Reviews";
+import { buttonVariants } from "@/components/ui/button";
+import MaxWidthWrapper from "./components/MaxWidthWrapper";
+import TShirt from "./components/TShirt";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -197,7 +199,75 @@ export default function Home() {
         </MaxWidthWrapper>
 
         {/* Animated review section */}
-        <Reviews />
+        <div className="pt-16">
+          <Reviews />
+        </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                Upload your photo and get{" "}
+                <span className="relative px-2 bg-orange-600 text-white">
+                  your own T-shirt{" "}
+                </span>{" "}
+                now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img
+                src="/arrow.png"
+                alt="arrow"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+              />
+
+              <div className="relative h-80 md:h-full w-[75%] md:justify-self-end max-w-screen-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-xl">
+                <img
+                  src="/horse.jpg"
+                  alt="horse"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                />
+              </div>
+              <TShirt
+                className="w-[450px] relative"
+                imgSrc="/horse_phone.jpg"
+                bigSize={true}
+              />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-orange-600 inline mr-1.5" />
+              High-quality cotton material
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-orange-600 inline mr-1.5" />
+              Unisex
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-orange-600 inline mr-1.5" />5 year
+              print warranty
+            </li>
+            <div className="flex justify-center">
+              <Link
+                className={buttonVariants({
+                  size: "lg",
+                  className: "mx-auto mt-8 font-semibold",
+                })}
+                href="/configure/upload"
+              >
+                Create your T-shirt now{" "}
+                <ArrowRight className="h-4 w-4 ml-1.5" />
+              </Link>
+            </div>
+          </ul>
+        </MaxWidthWrapper>
       </section>
     </div>
   );
